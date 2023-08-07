6b5a1c8f0bf5e06978059a8cbb6cd656bae748c9
harshdemon <137164844+harshdemon@users.noreply.github.com>
2023-08-07 13:40:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ for SESSION in SESSIONS:
             session_name=SESSION,
             api_id=API_ID,
             api_hash=API_HASH,
-            plugins=dict(root="TheXSpam"),
+            plugins=dict(root="ᕼᗩᖇꇙᕼᑌXD"),
         )
         CLIENTS.append(client)
 
@@ -20,11 +20,11 @@ if __name__ == "__main__":
     for i, CLIENT in enumerate(CLIENTS):
         try:
             CLIENT.start()
-            CLIENT.join_chat("TheAltron")
-            CLIENT.join_chat("AltronChats")
+            CLIENT.join_chat("Wrostdemon")
+            CLIENT.join_chat("HarshuXDBots")
             print(f"---> Client {i+1} has been Started...")
         except Exception as e:
             print(e)
 
-    print("💞YOUR PY-ALTRON SPAM USERBOTS DEPLOYED SUCCESSFULLY 💞")
+    print("💞YOUR ᕼᗩᖇꇙᕼᑌˣᴰ SPAM USERBOTS DEPLOYED SUCCESSFULLY 💞")
     idle()
